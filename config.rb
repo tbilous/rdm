9fe592e2e360cf92d185b9a1bb054cd08047a6a7
Taras Bilous <tbilous@yahoo.com>
2015-10-05 10:26:27 +0300

    First commit

---
+++ config.rb
@@ -0,0 +1,13 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "scripts"
+sourcemap     = true
+output_style  = :compressed
+sass_options  = { cache: false }
+line_comments = true
